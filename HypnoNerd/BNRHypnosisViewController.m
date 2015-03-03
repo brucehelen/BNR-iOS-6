56a45f943f7e4e5b25ab4cf816114e01376e1589
朱正晶 <zhuzhengjing@zhuzhengjingdeMac.local>
2015-03-03 17:50:52 +0800

    备份

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -9,29 +9,50 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisViewController()<UITextFieldDelegate>
+@property (nonatomic, strong)UITextField *textField;
+@end
+
 @implementation BNRHypnosisViewController
 
 // 使用代码的方式设置视图控制器的view属性
 - (void)loadView
 {
+    //UIView
+    //UIWindow
     // 先获取主屏幕的bounds，然后赋值给view，否则在下面的
     // viewDidLoad方法中，frame的值为0
     CGRect frame = [UIScreen mainScreen].bounds;
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
     
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    _textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    _textField.borderStyle = UITextBorderStyleRoundedRect;
+    _textField.placeholder = @"Hypnotize me";
+    _textField.returnKeyType = UIReturnKeyDone;
+    _textField.autocorrectionType = NO;
+    _textField.autocapitalizationType = UITextAutocapitalizationTypeAllCharacters;
+    _textField.delegate = self;
+    [backgroundView addSubview:_textField];
+    
     self.view = backgroundView;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"textField = %@", textField.text);
+    [_textField resignFirstResponder];
+    return YES;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    NSLog(@"BNRHypnosisViewController viewDidLoad, self.view = %@", self.view);
 }
+
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    NSLog(@"BNRHypnosisViewController initWithNibName");
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
 
     if (self) {
